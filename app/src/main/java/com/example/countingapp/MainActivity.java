437d2f6a30a913404daf46c0d40b426d4c8213ba
ben <jammydude7@googlemail.com>
2020-10-11 09:06:54 +0100

    set min/max for counter and fixed bug where users break the app if they type in number outside set limit

--- app/src/main/java/com/example/countingapp/MainActivity.java
+++ app/src/main/java/com/example/countingapp/MainActivity.java
@@ -2,17 +2,12 @@ package com.example.countingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.text.Editable;
-import android.text.SpannableStringBuilder;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,50 +32,61 @@ public class MainActivity extends AppCompatActivity {
         counter = findViewById(R.id.Number);
 
         // --Plus button--
-        // Increases the count by one per click
+        // Increases the count by one but stops the count at 9999
         // Flashes the background green when clicked
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 currentCount = Integer.parseInt(counter.getText().toString());
-                currentCount++;
-                counter.setText(currentCount + "");
-                background.setBackgroundColor(Color.parseColor("#51b46d"));
-                flash();
+
+                if(currentCount == 9999) {
+                    counter.setText(currentCount + "");
+                    numberLimitFlash();
+                }else{
+                    // If plus is clicked at 9999 limit
+                    currentCount++;
+                    counter.setText(currentCount + "");
+                    background.setBackgroundColor(Color.parseColor("#51b46d"));
+                    flash();
+                }
             }
         });
 
         // --Minus button--
         // Decreases the count by one but stops the count going below 0
-        // Flashes the background red when clicked and black when trying to go below zero
+        // Flashes the background red when clicked
         minus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(currentCount > 0) {
-                    currentCount = Integer.parseInt(counter.getText().toString());
+                currentCount = Integer.parseInt(counter.getText().toString());
+
+                if(currentCount == 0) {
+                    numberLimitFlash();
+                }else{
                     currentCount--;
                     counter.setText(currentCount + "");
-                   background.setBackgroundColor(Color.parseColor("#ff5147"));
-                   flash();
-
-                }else{
-                    // If minus is clicked when zero
-                    background.setBackgroundColor(Color.parseColor("#ffffff"));
-                    counter.setTextColor(Color.parseColor("#000000"));
-                    plus.setTextColor(Color.parseColor("#000000"));
-                    minus.setTextColor(Color.parseColor("#000000"));
-                    handler.postDelayed(new Runnable() {
-                        @Override
-                        public void run() {
-                            background.setBackgroundColor(Color.BLACK);
-                            counter.setTextColor(Color.parseColor("#ffffff"));
-                            plus.setTextColor(Color.parseColor("#ffffff"));
-                            minus.setTextColor(Color.parseColor("#ffffff"));
-                        }
-                    }, 200);
+                    background.setBackgroundColor(Color.parseColor("#ff5147"));
+                    flash();
                 }
             }
         });
+    }
+
+    // Used to flash white when user hits min(0)/max(9999) limit
+    private void numberLimitFlash() {
+        background.setBackgroundColor(Color.parseColor("#ffffff"));
+        counter.setTextColor(Color.parseColor("#000000"));
+        plus.setTextColor(Color.parseColor("#000000"));
+        minus.setTextColor(Color.parseColor("#000000"));
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                background.setBackgroundColor(Color.BLACK);
+                counter.setTextColor(Color.parseColor("#ffffff"));
+                plus.setTextColor(Color.parseColor("#ffffff"));
+                minus.setTextColor(Color.parseColor("#ffffff"));
+            }
+        }, 200);
     }
 
     // Used to create color flash effect
